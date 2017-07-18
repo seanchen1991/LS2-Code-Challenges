9b028b1caa82b772a30f32d050d9f129aaf54e33
Sean <seanchen11235@gmail.com>
2017-07-17 17:17:51 -0700

    Add empty function templates for all exercises

--- callBackPractice/callBackPractice.js
+++ callBackPractice/callBackPractice.js
@@ -18,39 +18,47 @@
 // or you can copy and paste your functions into the Chrome console or JSBin for testing.
 // console.log will be your best friend here!
 
-// Write a function called firstItem that passes the first item of the given array to the callback function
 
 const foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
-// write code here
+
+// Write a function called firstItem that passes the first item of the given array to the callback function
+const firstItem = (arr, cb) => {
+  // write code here
+};
 
 // Write a function called getLength that passes the length of the array into the callback
-// write code here
+const getLength = (arr, cb) => {
+  // write code here
+};
 
 // Write a function called last which passes the last item of the array into the callback
 // write code here
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
-
-// write code here
+const sumNums = (x, y, cb) => {
+  // write code here
+}
 
 sumNums(5, 10, (sum) => {
   console.log(`The sum is ${sum}.`);
 });
 
 // Write a function called multiplyNums that adds two numbers and passes the result to the callback
-// write code here
+const multiplyNums = (x, y, cb) => {
+  // write code here
+};
 
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
-
-// write code here
-
+const contains = (arr, cb) => {
+  // write code here
+};
 
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 
 const removeDuplicates = (arr, cb) => {
-
+  // write code here
 };
 
 removeDuplicates(foods, (uniqueFoods) => {
@@ -58,7 +66,6 @@ removeDuplicates(foods, (uniqueFoods) => {
 });
 
 // Write a function called each that iterates over the provided array and passes the value and index into the callback.
-
 const each = (arr, cb) => {
 
 };
